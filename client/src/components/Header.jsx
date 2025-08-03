a22b5d101c958d9776af121dd288bf661c997a61
AnshikSuhane <anshiksuhane9@gmail.com>
2025-08-03 23:52:27 +0530

    Another Commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,9 +17,10 @@ const Header = () => {
           </p>
         </div>
         <a
-          href=""
- className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"        >
-          Book Appointment{" "}
+        href="#speciality"
+          className="inline-flex items-center gap-2 bg-white px-5 py-2 rounded-full text-gray-600 text-sm font-medium hover:scale-105 transition-all duration-300 self-start"
+        >
+          Book Appointment
           <img src={assets.arrow_icon} alt="" className="w-3" />
         </a>
       </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import Speciality from '../components/speciality'
+import TopDoctor from '../components/TopDoctor'
 
 const Home = () => {
   return (
-    <div><Header /></div>
+    <div><Header />
+    <Speciality />
+    <TopDoctor />
+    </div>
   )
 }
 
